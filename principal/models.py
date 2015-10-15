c0fcbd5ebe083b4a459a2a3f092c773998b6d4c3
mcalvino78@gmail.com <mcalvino78@gmail.com>
2015-10-14 23:06:59 -0300

    Modificacion de fechas

--- principal/models.py
+++ principal/models.py
@@ -7,29 +7,30 @@ class Asociado(models.Model):
 	class Meta:
 		verbose_name_plural="Asociados"
 	nombre = models.CharField(max_length=500)
-	fecha_ingreso=models.DateTimeField(default=timezone.now)
+	fecha_ingreso=models.DateField(default=timezone.now)
 
 	def __unicode__(self):
-		return self.nombre		
+		return self.nombre
 
 class Vacacion(models.Model):
 	"""Modelo para las vacaciones"""
 	class Meta:
 		verbose_name_plural="Vacaciones"
 	asociado = models.ForeignKey(Asociado)
-	fecha_desde = models.DateTimeField(default=timezone.now)
-	fecha_hasta = models.DateTimeField(default=timezone.now)
+	fecha_desde = models.DateField(default=timezone.now)
+	fecha_hasta = models.DateField(default=timezone.now)
 
 	def __unicode__(self):
-		return self.asociado.nombre, self.fecha_hasta
+		return self.asociado.nombre + " : " + str(self.fecha_desde) + " to " + str(self.fecha_hasta)
 
 class Feriado(models.Model):
 	"""Modelo para los Feriados"""
 	class Meta:
 		verbose_name_plural="Feriados"
-	fecha = models.DateTimeField()
+	fecha = models.DateField()
 	descripcion = models.CharField(max_length=500)
-	tipo_feriado = models.CharField(max_length=10)
+	TIPOS = (('F', 'Fijo'), ('V' , 'Variable'))
+	tipo_feriado = models.CharField(max_length=8, choices=TIPOS, default=TIPOS[0])
 
 	def __unicode__(self):
-		return self.descripcion		
+		return self.descripcion
