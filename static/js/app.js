59bc1b7e37c9ed59258729c48f5029f9a901bd7c
andres2k <macalvino@hotmail.com>
2016-01-05 14:36:31 -0300

    add some templates

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function() {
+    $('#verVacaciones').click(function() {
+         alert("Hola mundo")
+    });
+});*/
+(function(){
+    var app = angular.module('vacaciones', []);
+
+    app.controller('vacacionController', function(){
+        alert("Hola mundo");
+    });
+})();
